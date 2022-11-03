5d2d081a95d74fe985ea30874253775a6d50a91b
rodriguesjeff <jefferson.rodrigues@seventh.com.br>
2022-11-03 13:29:32 -0300

    Projeto quase pronto

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:curso_flutter_ifpe/src/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,70 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          FloatingActionButton(
-            onPressed: _decrementCounter,
-            tooltip: 'Increment',
-            child: const Icon(Icons.remove),
-          ),
-          FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          ),
-        ],
-      ),
+      home: const HomePage(),
     );
   }
 }
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController dollarController = TextEditingController();
+  TextEditingController euroController = TextEditingController();
+  TextEditingController realController = TextEditingController();
+  double dollarCotacao = 0.0;
+  double euroCotacao = 0.0;
+  double real = 0.0;
+  double dollarTotal = 0.0;
+  double euroTotal = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: const Text('Conversor de moedas'),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (c) {
+              return AlertDialog(
+                title: const Center(
+                  child: Text(
+                    'Adicionar cotação',
+                  ),
+                ),
+                content: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: dollarController,
+                        decoration: const InputDecoration(
+                          label: Text('Dollar'),
+                        ),
+                      ),
+                      TextField(
+                        controller: euroController,
+                        decoration: const InputDecoration(
+                          label: Text('Euro'),
+                        ),
+                      ),
+                      const SizedBox(height: 25.0),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            dollarCotacao =
+                                double.tryParse(dollarController.text)!;
+                            euroCotacao = double.tryParse(euroController.text)!;
+                          });
+                          Navigator.pop(context);
+                        },
+                        child: const Text('ADICIONAR'),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+        child: const Icon(
+          Icons.currency_exchange_rounded,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text('Dólar Hoje: ${dollarCotacao.toStringAsPrecision(2)}'),
+                  Text('Euro Hoje: ${euroCotacao.toStringAsPrecision(2)}'),
+                ],
+              ),
+              const SizedBox(height: 25.0),
+              const Text('SEU SALDO: '),
+              const SizedBox(height: 25.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text('Dólares: ${dollarTotal.toStringAsPrecision(2)}'),
+                  Text('Euros: ${euroTotal.toStringAsPrecision(2)}'),
+                ],
+              ),
+              const SizedBox(height: 25.0),
+              TextField(
+                controller: realController,
+                onChanged: (value) {
+                  setState(() {
+                    if (realController.text.isNotEmpty) {
+                      dollarTotal =
+                          double.tryParse(realController.text)! * dollarCotacao;
+                      euroTotal =
+                          double.tryParse(euroController.text)! * euroCotacao;
+                    }
+                  });
+                },
+                decoration: const InputDecoration(
+                  label: Text('Real'),
+                  prefix: Text('R\$ '),
+                ),
+              ),
+              const SizedBox(height: 25.0),
+              ElevatedButton(
+                onPressed: () {},
+                style: const ButtonStyle().copyWith(
+                  backgroundColor: MaterialStateProperty.all(
+                    Colors.amber,
+                  ),
+                ),
+                child: const Text(
+                  'CALCULAR',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
